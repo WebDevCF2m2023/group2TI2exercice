7aaba6d4d82cbd3eceaf68eef68b0e9b12ade3e2
PommeDeuxTerre <lemonniergregory2@gmail.com>
2024-02-12 16:06:42 +0100

    add models functions

--- Modeles/informationsModel.php
+++ Modeles/informationsModel.php
@@ -1,12 +1,20 @@
 <?php
 // Notre fonction qui charge les informations
-function getInformations(PDO $db)
-{
-
+function getInformations(PDO $db):array{
+    $sql = "SELECT * FROM `informations`";
+    $query = $db->query($sql);
+    $results = $query->fetchAll(PDO::FETCH_ASSOC);
+    $query->closeCursor();
+    return $results;
 }
 
 // notre fonction qui insert dans informations
-function addInformations(PDO $db)
-{
-    
+function addInformations(PDO $db, string $mail, string $message){
+    $sql = "INSERT INTO `informations`(`themail`, `themessage`) VALUES(?,?)";
+    $prepare = $db->prepare($sql);
+    $prepare->bindValue(1,$mail,PDO::PARAM_STR);
+    $prepare->bindValue(2,$message,PDO::PARAM_STR);
+    $prepare->execute();
+    $results = $prepare->fetchAll(PDO::FETCH_ASSOC);
+    return $results;
 }
