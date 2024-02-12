3a1eb8ee8c765b7e08e5092cc04e8d52b532b84b
PommeDeuxTerre <lemonniergregory2@gmail.com>
2024-02-12 16:07:01 +0100

    finish routeur

--- public/index.php
+++ public/index.php
@@ -1,16 +1,23 @@
 <?php
 
 // Chargement des dépendances
+require_once("../config.php");
+require_once("../Modeles/informationsModel.php");
 
 // Connexion à la base de donnée
+$dsn = MY_DB_DRIVER.":host=".MY_DB_HOST.";dbname=".MY_DB_NAME.";charset=".MY_DB_CHARSET.";port=".MY_DB_PORT;
+$db_connect = new PDO($dsn, MY_DB_LOGIN, MY_DB_PWD);
 
 // Si le formulaire a été envoyé
-
+if (!empty($_POST["mail"]) && !empty($_POST["message"])){
     // On insert dans la table `informations` si valide
-
-// on récupère toutes les entrées de la table
+    addInformations($db_connect, $_POST["mail"], $_POST["message"]);
+}
 // `informations`
+$informations = getInformations($db_connect);
 
 // on charge le template qui affiche la vue
+include("../Vues/informations.vue.html.php");
 
 // on ferme la connexion 
+$db_connect = null;
