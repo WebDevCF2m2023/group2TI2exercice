233118190b280b99a6b12ae5817acca82b6b1b41
simonaiuliana <Iuliana.hoater@cf2000.onmicrosoft.com>
2024-02-14 11:37:36 +0100

    modiffied files in model vues and public

--- Vues/informations.vue.html.php
+++ Vues/informations.vue.html.php
@@ -6,6 +6,39 @@
     <title>Informations</title>
 </head>
 <body>
+    <?php foreach($informations as $information):?>
+        <div class="information">
+            <p><?=$information["thedate"]?>: <?=$information["themessage"]?></p>
+        </div>
+    <?php endforeach?>
+
+
+    <h1></h1>
+    <form action="" method="post" >
+    <div id="themail"><br><br>    
+        <label for="nom">Mail</label>
+        <input type="text" name="nom" id="nom"><br><br>
+    </div>
+
+
     
+        
+   
+
+    <div id="themessage">
+        <label for="message">Message</label><br>
+        <textarea id="message" name="message" rows="10" cols="30" maxlength="1024"></textarea><br><br>
+        
+    </div>
+
+
+   
+
+    <div id="lesubmit">
+     <input type="submit" value="envoyer les données"><br><br>
+    </div> 
+    </form>
+    
+
 </body>
 </html>
