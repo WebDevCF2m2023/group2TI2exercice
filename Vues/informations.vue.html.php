7f1823f9d6a4cdd6621a34a448fe8bd819914b45
PommeDeuxTerre <lemonniergregory2@gmail.com>
2024-02-14 11:46:30 +0100

    finish front end

--- Vues/informations.vue.html.php
+++ Vues/informations.vue.html.php
@@ -4,12 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Informations</title>
+    <link rel="stylesheet" href="/css/MyCSS.css">
 </head>
 <body>
+    <form action="./" method="post">
+        <div class="field">
+            <label for="email">email</label>
+            <input type="email" name="mail" id="mail">
+        </div>
+        <div class="field">
+            <label for="message">message</label>
+            <textarea name="message" id="message"></textarea>
+        </div>
+        <div class="field">
+            <input type="submit" value="envoyer">
+        </div>
+    </form>
+    <div id="messages">
     <?php foreach($informations as $information):?>
-        <div class="information">
-            <p><?=$information["thedate"]?>: <?=$information["themessage"]?></p>
+        <div class="message">
+            <h2><?=$information["thedate"]?></h2>
+            <p><?=$information["themessage"]?></p>
         </div>
     <?php endforeach?>
+    </div>
 </body>
 </html>
