1ed2037c9d0ef2a02034da08c53ce21630ce261c
EEnez <enezgubeljic@gmail.com>
2024-02-15 08:36:48 +0100

    majTI

--- Vues/informations.vue.html.php
+++ Vues/informations.vue.html.php
@@ -1,11 +1,51 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Informations</title>
+    <title>Document</title>
+    <link rel="stylesheet" href="css/MyCSS.css">
 </head>
 <body>
-    
+<?php
+        foreach ($informations as $commentaire) {
+            ?>
+            <div class="card">
+                <div class="card-header">
+                <p>
+  <?php echo $commentaire['thedate']; ?> | <?php echo htmlspecialchars($commentaire['themessage']); ?> | <?php echo htmlspecialchars($commentaire['themail']); ?>
+</p>
+                </div>
+                <div class="card-body">
+                    <h5><?php echo $commentaire['thedate']; ?></h5>
+                    <p><?php echo $commentaire['themail']; ?></p>
+                    <p><?php echo $commentaire['themessage']; ?></p>
+                    
+                </div>
+            </div>
+            <?php
+        }
+        ?>
+    <h1>Formulaire de contact</h1>
+    <form action="" method="post">
+        <div>
+            <div id="lenom">
+                <label for="nom">Nom</label>
+                <input type="email" name="themail" id="nom">
+            </div>
+
+            
+
+        <div>
+            <div id="lemessage">
+                <label for="msg">Message</label>
+                <textarea name="themessage" id="msg" cols="30" rows="5" maxlength="1024"></textarea>
+            </div>
+        </div>
+        <div id="envoi">
+            <input type="submit" value="Envoyer les données">
+        </div>
+    </form>
 </body>
 </html>
+<?php var_dump($_POST);
