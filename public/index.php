8b4cf122649806b8a0639c38ae650b129a6a155f
WebDevCF2m <gitweb@cf2m.be>
2024-02-15 15:16:48 +0100

    dependencies

--- public/index.php
+++ public/index.php
@@ -1,8 +1,16 @@
 <?php
 
 // Chargement des dépendances
+require_once "../config.php";
+require_once "../Modeles/informationsModel.php";
+// Connexion à la base de donnée en PDO
 
-// Connexion à la base de donnée
+try{
+    $MyPDO = new PDO(MY_DB_DRIVER.":host=".MY_DB_HOST.";dbname=".MY_DB_NAME.";port=".MY_DB_PORT.";charset=".MY_DB_CHARSET, MY_DB_LOGIN, MY_DB_PWD);
+
+}catch(Exception $e){
+    die($e->getMessage());
+}
 
 // Si le formulaire a été envoyé
 
@@ -10,7 +18,9 @@
 
 // on récupère toutes les entrées de la table
 // `informations`
+var_dump(getInformations($MyPDO));
 
 // on charge le template qui affiche la vue
+include "../Vues/informations.vue.html.php";
 
 // on ferme la connexion 
