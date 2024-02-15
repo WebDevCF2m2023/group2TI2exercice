225a32ab2ee9d89f71705ada5f4d8108a1a4f23d
WebDevCF2m <gitweb@cf2m.be>
2024-02-15 15:17:07 +0100

    getInformations() ok

--- Modeles/informationsModel.php
+++ Modeles/informationsModel.php
@@ -1,8 +1,12 @@
 <?php
 // Notre fonction qui charge les informations
-function getInformations(PDO $db)
+function getInformations(PDO $db): array
 {
-
+    $sql = "SELECT * FROM informations ORDER BY thedate DESC";
+    $query = $db->query($sql);
+    $result = $query->fetchAll(PDO::FETCH_ASSOC);
+    $query->closeCursor();
+    return $result;
 }
 
 // notre fonction qui insert dans informations
